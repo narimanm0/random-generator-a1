944731025da2f4f10bcf4257aebca4b620dd430e
Nariman Mammadov <nmammadov16237@ada.edu.az>
2026-02-09 11:10:58 +0400

    Initial submission: class structure and execution flow - Added base class definitions - Implemented main method - Established basic execution flow Further functionality will be introduced incrementally

---
+++ Generator.java
@@ -0,0 +1,30 @@
+/*
+    Name: Nariman Mammadov
+    Project: Random Generator 
+    Class: CSCI-3612 - 20964
+    Date: 09.02.2026
+*/
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+// CLASS DEFINITION
+public class Generator { 
+
+    // CLASS ATTRIBUTE
+    private int totalNumbersGenerated = 0;
+
+    // METHOD DEFINITION
+    public void execute() {
+
+    }
+
+    
+    // Program entry point.
+    public static void main(String[] args) {
+        // OBJECT INSTANTIATION
+        Generator g = new Generator();
+        g.execute();
+    }
+}
